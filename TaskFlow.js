fdac93c0857a2d164d75b339d27585001ab0f2be
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-10-31 15:19:44 +0100

    "Updated HTML, CSS, and JavaScript files with various changes to layout, styling, and functionality."

--- TaskFlow.js
+++ TaskFlow.js
@@ -3,6 +3,33 @@ const addTask = document.querySelector("#addTask");
 const popup = document.querySelector("#popup");
 const confirmButton = document.querySelector("#confirmButton");
 const closeButton = document.querySelector("#closeBUtton");
+// this place for selecting all the inputs 
+const taskTitle = document.querySelector("#taskTitile");
+const dueDate = document.querySelector("#date");
+const priority = document.querySelector("#priority");
+const status = document.querySelector("#status");
+const Discreption = document.querySelector("#Discreption");
+// select the error containers of the input fields
+const titleError = document.querySelector("#titleError");
+const dateError = document.querySelector("#dateError")
+// select the date to compare it later
+const today = new Date;
+today.setHours(0,0,0,0); //this is seting the hour to med night 
+
+
+
+
+
+
+
+const myTaskes = JSON.parse(localStorage.getItem("task")) || [];
+const newtask = {
+    title: "",
+    dueDate: "",
+    priority: "",
+    status: "",
+    Discreption: "",
+};
 
 
 // an evnt that shows the pop up when the button is clicked 
@@ -12,16 +39,36 @@ addTask.addEventListener("click",(e)=>{
     addTask.setAttribute("disabled","");
     popup.classList.add("block");
     popup.classList.remove("hidden");
-})
-console.log(addTask);
-
+});
 closeButton.addEventListener("click",(e)=>{
     e.preventDefault;
     popup.classList.add("hidden");
     popup.classList.remove("block");
-})
+    addTask.removeAttribute("disabled")
+});
 confirmButton.addEventListener("click",(e)=>{
     e.preventDefault;
+    if(taskTitle.value.length<6){
+        const titileError = `<p class="text-red-500 mx-auto">your title must be at least 6 characters long</p>`;
+        titleError.innerHTML = titileError;
+        return;
+    } else{
+        titleError.innerHTML = "";
+    }
+    const dueDatevalue = new Date(dueDate.value);
+    
+    if (dueDatevalue < today) {
+        const dateErroR =`<p class="text-red-500 mx-auto">you'r due date is in the past</p>`;
+        dateError.innerHTML = `<p class="text-red-500 mx-auto">your title must be at least 6 characters long</p>`
+        return
+    }
+    else{
+        dateError.innerHTML = "";
+    }
+    
+
+
     popup.classList.add("hidden");
     popup.classList.remove("block");
-})
+    addTask.removeAttribute("disabled");
+});
